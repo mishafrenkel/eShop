2764075d5a52909c5f69d58a5243eb6cf7615c1a
Michael Frenkel <michael.frenkel@outlook.com>
2019-07-27 17:44:50 -0700

    init

---
+++ start.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+// import environmental variables from our variables.env file
+require('dotenv').config({ path: 'variables.env' });
+
+// Connect to our Database and handle any bad connectionsåå
+
+mongoose.connect(process.env.DATABASE)
+  .then(() => console.log('connecting to Mongo Atlas Cluster'))
+  .catch(e => console.error(e));
+
+//   mongoose.connection.on('error', (err) => {
+//   console.error(`🙅 🚫 🙅 🚫 🙅 🚫 🙅 🚫 → ${err.message}`);
+// });
+
+// READY?! Let's go!
+
+// import all of our models
+require('./models/Store');
+require('./models/User');
+require('./models/Review');
+
+// Start our app!
+const app = require('./app');
+app.set('port', process.env.PORT || 7777);
+const server = app.listen(app.get('port'), () => {
+  console.log(`Express running → PORT ${server.address().port}`);
+});
